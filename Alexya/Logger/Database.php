4a727f63bbba60324e47d4473550e0ded84c1413
Manulaiko <manulaiko@gmail.com>
2017-06-05 16:03:33 +0200

    Minor improvements, docblock formatting

--- Alexya/Logger/Database.php
+++ Alexya/Logger/Database.php
@@ -2,6 +2,7 @@
 namespace Alexya\Logger;
 
 use \Alexya\Database\Connection;
+use Alexya\Database\QueryBuilder;
 
 /**
  * Alexya's Database Logger.
@@ -10,10 +11,9 @@ use \Alexya\Database\Connection;
  *
  * The constructor accepts as parameter the following parameters:
  *
- *  * The [\Alexya\Database\Connection](../../vendor/alexya-framework/filesystem/Alexya/Database/Connection.php)
- *    object that will be used for interacting with the database.
+ *  * The `\Alexya\Database\Connection` object that will be used for interacting with the database.
  *  * A string being the table name.
- *  * An associative array containing the columns and the values to insert, you can insert the followin placeholders:
+ *  * An associative array containing the columns and the values to insert, you can insert the following placeholders:
  *      * `{YEAR}`, current year.
  *      * `{MONTH}`, current month.
  *      * `{DAY}`, current day.
@@ -26,8 +26,8 @@ use \Alexya\Database\Connection;
  *      * `{CALLING_CLASS}`, the class that called the logger.
  *      * `{CALLING_TYPE}`, `->` if the logger was called by an object, `::` if it was called statically.
  *      * `{LEVEL}`, the level on which the log has been called.
- *      * `{LOG}`, the string to log
- *  * A string being the format that each log entry will have, you can add the followin placeholders:
+ *      * `{LOG}`, the string to log.
+ *  * A string being the format that each log entry will have, you can add the following placeholders:
  *      * `{YEAR}`, current year.
  *      * `{MONTH}`, current month.
  *      * `{DAY}`, current day.
@@ -40,15 +40,14 @@ use \Alexya\Database\Connection;
  *      * `{CALLING_CLASS}`, the class that called the logger.
  *      * `{CALLING_TYPE}`, `->` if the logger was called by an object, `::` if it was called statically.
  *      * `{LEVEL}`, the level on which the log has been called.
- *      * `{LOG}`, the string to log
+ *      * `{LOG}`, the string to log.
  *  * An array containing the elements that will be logged, you can get a full list
- *    of available values in the class [\Psr\Log\LogLevel](../../vendor/psr/log/Psr/Log/LogLevel)
+ *    of available values in the class `\Psr\Log\LogLevel`
  *
- * The method `log` performs the actual loggin and accepts as parameter the log level
- * (see [\Psr\Log\LogLevel](../../vendor/psr/log/Psr/Log/LogLevel) for a list of possibe values) and the
- * string to log.
+ * The method `log` performs the actual logging and accepts as parameter the log level
+ * (see `\Psr\Log\LogLevel` for a list of possible values) and the string to log.
  *
- * There are also 8 methods for loggin in a specific category:
+ * There are also 8 methods for logging in a specific category:
  *  * `emergency`
  *  * `alert`
  *  * `critical`
@@ -62,34 +61,36 @@ use \Alexya\Database\Connection;
  *
  * Example:
  *
- *     $Logger = new Logger(
- *         $Database,
- *         "logs",
- *         [
- *             "date"    => "{YEAR}-{MONTH}-{DAY} {HOUR}:{MINUTE}:{SECOND}",
- *             "caller"  => "{CALLER_CLASS}{CALLER_TYPE}{CALLER_FUNCTION} ({CALLER_FILE}:{CALLER_LINE})",
- *             "level"   => "{LEVEL}",
- *             "message" => "{LOG}"
- *         ],
- *         [
- *             \Psr\Log\LogLevel::EMERGENCY,
- *             \Psr\Log\LogLevel::ALERT,
- *             \Psr\Log\LogLevel::CRITICAL,
- *             \Psr\Log\LogLevel::ERROR,
- *             \Psr\Log\LogLevel::WARNING,
- *             \Psr\Log\LogLevel::NOTICE,
- *             \Psr\Log\LogLevel::INFO,
- *             \Psr\Log\LogLevel::DEBUG
- *         ]
- *     );
- *
- *     $Logger->debug("test"); // INSERT INTO `logs` (`date`, `caller`, `level`, `message`) VALUES ('0000-00-00 00:00:00', '', 'debug', 'test');
- *     $Logger->info("test", [
- *         "date"    => "{HOUR}:{MINUTE}:{SECOND}",
+ * ```php
+ * $Logger = new Logger(
+ *     $Database,
+ *     "logs",
+ *     [
+ *         "date"    => "{YEAR}-{MONTH}-{DAY} {HOUR}:{MINUTE}:{SECOND}",
  *         "caller"  => "{CALLER_CLASS}{CALLER_TYPE}{CALLER_FUNCTION} ({CALLER_FILE}:{CALLER_LINE})",
  *         "level"   => "{LEVEL}",
  *         "message" => "{LOG}"
- *     ]); // INSERT INTO `logs` (`date`, `caller`, `level`, `message`) VALUES ('00:00:00', '', 'debug', 'test');
+ *     ],
+ *     [
+ *         \Psr\Log\LogLevel::EMERGENCY,
+ *         \Psr\Log\LogLevel::ALERT,
+ *         \Psr\Log\LogLevel::CRITICAL,
+ *         \Psr\Log\LogLevel::ERROR,
+ *         \Psr\Log\LogLevel::WARNING,
+ *         \Psr\Log\LogLevel::NOTICE,
+ *         \Psr\Log\LogLevel::INFO,
+ *         \Psr\Log\LogLevel::DEBUG
+ *     ]
+ * );
+ *
+ * $Logger->debug("test"); // INSERT INTO `logs` (`date`, `caller`, `level`, `message`) VALUES ('0000-00-00 00:00:00', '', 'debug', 'test');
+ * $Logger->info("test", [
+ *     "date"    => "{HOUR}:{MINUTE}:{SECOND}",
+ *     "caller"  => "{CALLER_CLASS}{CALLER_TYPE}{CALLER_FUNCTION} ({CALLER_FILE}:{CALLER_LINE})",
+ *     "level"   => "{LEVEL}",
+ *     "message" => "{LOG}"
+ * ]); // INSERT INTO `logs` (`date`, `caller`, `level`, `message`) VALUES ('00:00:00', '', 'debug', 'test');
+ * ```
  *
  * @author Manulaiko <manulaiko@gmail.com>
  */
@@ -98,7 +99,7 @@ class Database extends AbstractLogger
     /**
      * The database object.
      *
-     * @var \Alexya\Database\Connection
+     * @var Connection
      */
     private $_database = null;
 
@@ -107,7 +108,7 @@ class Database extends AbstractLogger
      *
      * @var string
      */
-    private $_table_name = "logs";
+    private $_table = "logs";
 
     /**
      * The array containing the columns and the values to insert.
@@ -115,42 +116,45 @@ class Database extends AbstractLogger
      * @var string
      */
     private $_columns = [
-                "date"    => "{YEAR}-{MONTH}-{DAY}",
-                "caller"  => "{CALLER_CLASS}{CALLER_TYPE}{CALLER_FUNCTION} ({CALLER_FILE}:{CALLER_LINE})",
-                "level"   => "{LEVEL}",
-                "message" => "{LOG}"
-            ];
+            "date"    => "{YEAR}-{MONTH}-{DAY}",
+            "caller"  => "{CALLER_CLASS}{CALLER_TYPE}{CALLER_FUNCTION} ({CALLER_FILE}:{CALLER_LINE})",
+            "level"   => "{LEVEL}",
+            "message" => "{LOG}"
+    ];
+
     /**
-     * Constructor
+     * Constructor.
      *
      * Example:
      *
-     *     $Logger = new \Alexya\Logger\Database(
-     *         $Database,
-     *         "logs",
-     *         [
-     *             "date"    => "{YEAR}-{MONTH}-{DAY} {HOUR}:{MINUTE}:{SECOND}",
-     *             "caller"  => "{CALLER_CLASS}{CALLER_TYPE}{CALLER_FUNCTION} ({CALLER_FILE}:{CALLER_LINE})",
-     *             "level"   => "{LEVEL}",
-     *             "message" => "{LOG}"
-     *         ],
-     *         [
-     *             \Psr\Log\LogLevel::EMERGENCY,
-     *             \Psr\Log\LogLevel::ALERT,
-     *             \Psr\Log\LogLevel::CRITICAL,
-     *             \Psr\Log\LogLevel::ERROR,
-     *             \Psr\Log\LogLevel::WARNING,
-     *             \Psr\Log\LogLevel::NOTICE,
-     *             \Psr\Log\LogLevel::INFO,
-     *             \Psr\Log\LogLevel::DEBUG
-     *         ]
-     *     );
+     * ```php
+     * $Logger = new Logger(
+     *     $Database,
+     *     "logs",
+     *     [
+     *         "date"    => "{YEAR}-{MONTH}-{DAY} {HOUR}:{MINUTE}:{SECOND}",
+     *         "caller"  => "{CALLER_CLASS}{CALLER_TYPE}{CALLER_FUNCTION} ({CALLER_FILE}:{CALLER_LINE})",
+     *         "level"   => "{LEVEL}",
+     *         "message" => "{LOG}"
+     *     ],
+     *     [
+     *         \Psr\Log\LogLevel::EMERGENCY,
+     *         \Psr\Log\LogLevel::ALERT,
+     *         \Psr\Log\LogLevel::CRITICAL,
+     *         \Psr\Log\LogLevel::ERROR,
+     *         \Psr\Log\LogLevel::WARNING,
+     *         \Psr\Log\LogLevel::NOTICE,
+     *         \Psr\Log\LogLevel::INFO,
+     *         \Psr\Log\LogLevel::DEBUG
+     *     ]
+     * );
+     * ```
      *
-     * @param \Alexya\Database\Connection $database   The database connection object.
-     * @param string                      $table_name The table where logs should be saved.
-     * @param array                       $columns    The array containing the rows and the values to insert.
-     * @param string                      $log_format The format of each log entry
-     * @param array                       $log_levels What levels should the logger log
+     * @param Connection $database   The database connection object.
+     * @param string     $table_name The table where logs should be saved.
+     * @param array      $columns    The array containing the rows and the values to insert.
+     * @param string     $log_format The format of each log entry.
+     * @param array      $log_levels What levels should the logger log.
      */
     public function __construct(
         Connection $database,
@@ -162,7 +166,7 @@ class Database extends AbstractLogger
         $this->_database = $database;
 
         if(!empty($table_name)) {
-            $this->_table_name = $table_name;
+            $this->_table = $table_name;
         }
         if(!empty($columns)) {
             $this->_columns = $columns;
@@ -172,32 +176,23 @@ class Database extends AbstractLogger
     }
 
     /**
-     * Writes the log message to the database table
+     * Writes the log message to the database table.
      *
-     * @param string $message Message to log
-     * @param array  $context Custom placeholders
+     * @param string $message Message to log.
+     * @param array  $context Custom placeholders.
      */
-    private function _write(string $message, array $context)
+    protected function _write(string $message, array $context = [])
     {
-        $placeholders = [
-            "CALLER_CLASS"    => $caller["class"],
-            "CALLER_FUNCTION" => $caller["function"],
-            "CALLER_FILE"     => $caller["file"]
-            "CALLER_TYPE"     => $caller["type"],
-            "CALLER_LINE"     => $caller["line"],
-            "LEVEL"           => $level,
-            "LOG"             => $message
-        ];
-
-        foreach($this->_columns as $key => $value) {
-            $this->_columns[$key] = $this->_parseContext($value, array_merge($placeholders, $this->_getDefaultPlaceholders()));
-        }
+        $query = new QueryBuilder($this->_database);
 
         // Insert a new row into database
-        $query = $Database->insert($this->_table)
-                          ->values($this->_columns)
-                          ->execute();
-        if(!is_numeric($query)) {
+        $query = $query->insert($this->_table)
+                       ->values($this->_columns)
+                       ->getQuery();
+
+        $result = $this->_database->insert($query);
+
+        if(!is_numeric($result)) {
             // Something went wrong, idk what to do here...
         }
     }
